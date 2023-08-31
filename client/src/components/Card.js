339b7392865dcc36daf9b8a6f0c439f2ced4ee07
Katerina Savinskaya <mega.savinskaya@gmail.com>
2023-08-31 12:34:53 +0300

    Add tasks on cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,45 @@
-import './Card.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function Card({day, style}) {
+import "./Card.css";
+
+function Card({ day, style }) {
+  const palette = {
+    red: "#FF3838",
+    green: "#6CFF38",
+    yellow: "#FFE455",
+    blue: "#687BE3"
+  }
+  const [tasks, setTasks] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:8080/card')
+      .then((response) => response.json()).catch(console.log("Katya"))
+      .then((json) => {
+        setTasks(json);
+      });
+  }, []);
+
+  function drawTasks(){
+    let section = []
+    for (let rect in tasks)
+    {
+      if(section.length === 3)
+        break
+      section.push(<div className="Rectangle" style={{backgroundColor: palette[tasks[rect].color]}}></div>)
+    
+    }
+    return section
+  }
+
   return (
-    <div className={style}>
-        <h5>{day}</h5>
+    <div
+      className={style}
+      onClick={() => {
+        console.log(day);
+      }}
+    >
+      <h5>{day}</h5>
+      {drawTasks()} 
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,52 @@
 require('dotenv').config
+
+const cors = require("cors");
 const express = require('express')  
+
+const data1 = {
+  1: {color: "yellow"},
+  2: {color: "red"}
+}
+
+const data2 = {
+  1: {color: "blue"},
+  2: {color: "yellow"},
+  3: {color: "red"},
+  4: {color: "green"},
+  5: {color: "green"},
+  6: {color: "green"},
+  7: {color: "green"},
+  8: {color: "green"},
+}
+
+const data3 = {
+}
 
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-app.use(express.json())
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.use(express.json(), cors({
+  origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+          var msg =
+              "The CORS policy for this site does not " +
+              "allow access from the specified Origin.";
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  }
+}))
+
+app.get('/card', (req, res) => {
+  const randoMassiv=[data1, data2, data3]
+  console.log(JSON.stringify(randoMassiv[Math.floor(Math.random()*2.4)]))
+  res.send(JSON.stringify(randoMassiv[Math.floor(Math.random()*2.4)]))
+})
 
 app.listen(PORT, (err) => {
     if(err)
